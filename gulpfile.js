0101989b2e971f48ccb2f8075744174f62d50b36
Victor Nystad <vnystad@gmail.com>
2014-12-13 00:26:06 +0100

    deaktiverte spreadsheets tasks

--- gulpfile.js
+++ gulpfile.js
@@ -54,121 +54,121 @@ gulp.task('watch', function () {
     }])
 });
 
-gulp.task('contacts', ['clean:contacts'], function () {
-    return new Promise(function (resolve, reject) {
-
-        return afk_sheet.getInfo(function (err, info) {
-
-            var contacts = info.worksheets[1];
-
-            if (contacts != undefined) {
-                resolve(contacts);
-            } else {
-                reject(new Error('no contacts'))
-            }
-        })
-    })
-    // .then(console.log.bind(console))
-    .then(function(data) {
-        var contacts = [];
-        data.getRows(1, function(err, rows) {
-            rows.forEach(function(row) {
-
-                useless.forEach(function (prop) {
-                    delete row[prop];
-                });
-
-                contacts.push(row);
-
-            });
-            file('contacts.json', JSON.stringify(contacts))
-                .pipe(convert({
-                    from: 'json',
-                    to: 'yml'
-                }))
-                // .pipe(console.log.bind(console))
-                .pipe(gulp.dest('_data'))
-        });
-    })
-    .catch(function(error) {
-        console.log(new Error(error))
-    })
-});
-
-
-gulp.task('teams', ['clean:teams'], function () {
-    return new Promise(function (resolve, reject) {
-        return afk_sheet.getInfo(function (err, info) {
-
-            var teams = info.worksheets[0];
-
-            if (teams != undefined) {
-                resolve(teams);
-            } else {
-                reject(new Error('no teams'))
-            }
-        })
-    })
-    .then(function(data) {
-
-        var teams = [];
-
-        data.getRows(1, function(err, rows) {
-
-            rows.forEach(function(row, i) {
-
-                useless.forEach(function (prop) {
-                    delete row[prop];
-                });
-
-                row.title = row.lagnavniturnering;
-
-                teams.push(row);
-
-                var filename = row.hovedlag.toLowerCase() + ' ' + row.lagnavniturnering.toLowerCase();
-
-                filename = filename.replace(/[æøå\/]/g, function(m) {
-                    return {
-                        '/' : '-',
-                        'æ' : 'a',
-                        'ø' : 'o',
-                        'å' : 'a'
-                    }[m];
-                }).replace(/\s/g, '-');
-
-
-                file(  filename + '.json', JSON.stringify(row))
-                    .pipe(convert({
-                        from: 'json',
-                        to: 'yml'
-                    }))
-                    .pipe(insert.wrap('---\nlayout: team\n', '---\n'))
-                    .pipe(replace('.md'))
-                    .pipe(gulp.dest('lag'))
-            });
-
-            file('teams.json', JSON.stringify(teams))
-                .pipe(convert({
-                    from: 'json',
-                    to: 'yml'
-                }))
-                // .pipe(console.log.bind(console))
-                .pipe(gulp.dest('_data'))
-
-        });
-    })
-    .catch(function(error) {
-        console.log(new Error(error))
-    })
-})
-
-
-gulp.task('clean:teams', function(cb) {
-    del(['./lag/**', './_data/teams.yml'], cb);
-});
-
-gulp.task('clean:contacts', function(cb) {
-    del(['./_data/contacts.yml'], cb);
-})
+// gulp.task('contacts', ['clean:contacts'], function () {
+//     return new Promise(function (resolve, reject) {
+//
+//         return afk_sheet.getInfo(function (err, info) {
+//
+//             var contacts = info.worksheets[1];
+//
+//             if (contacts != undefined) {
+//                 resolve(contacts);
+//             } else {
+//                 reject(new Error('no contacts'))
+//             }
+//         })
+//     })
+//     // .then(console.log.bind(console))
+//     .then(function(data) {
+//         var contacts = [];
+//         data.getRows(1, function(err, rows) {
+//             rows.forEach(function(row) {
+//
+//                 useless.forEach(function (prop) {
+//                     delete row[prop];
+//                 });
+//
+//                 contacts.push(row);
+//
+//             });
+//             file('contacts.json', JSON.stringify(contacts))
+//                 .pipe(convert({
+//                     from: 'json',
+//                     to: 'yml'
+//                 }))
+//                 // .pipe(console.log.bind(console))
+//                 .pipe(gulp.dest('_data'))
+//         });
+//     })
+//     .catch(function(error) {
+//         console.log(new Error(error))
+//     })
+// });
+//
+//
+// gulp.task('teams', ['clean:teams'], function () {
+//     return new Promise(function (resolve, reject) {
+//         return afk_sheet.getInfo(function (err, info) {
+//
+//             var teams = info.worksheets[0];
+//
+//             if (teams != undefined) {
+//                 resolve(teams);
+//             } else {
+//                 reject(new Error('no teams'))
+//             }
+//         })
+//     })
+//     .then(function(data) {
+//
+//         var teams = [];
+//
+//         data.getRows(1, function(err, rows) {
+//
+//             rows.forEach(function(row, i) {
+//
+//                 useless.forEach(function (prop) {
+//                     delete row[prop];
+//                 });
+//
+//                 row.title = row.lagnavniturnering;
+//
+//                 teams.push(row);
+//
+//                 var filename = row.hovedlag.toLowerCase() + ' ' + row.lagnavniturnering.toLowerCase();
+//
+//                 filename = filename.replace(/[æøå\/]/g, function(m) {
+//                     return {
+//                         '/' : '-',
+//                         'æ' : 'a',
+//                         'ø' : 'o',
+//                         'å' : 'a'
+//                     }[m];
+//                 }).replace(/\s/g, '-');
+//
+//
+//                 file(  filename + '.json', JSON.stringify(row))
+//                     .pipe(convert({
+//                         from: 'json',
+//                         to: 'yml'
+//                     }))
+//                     .pipe(insert.wrap('---\nlayout: team\n', '---\n'))
+//                     .pipe(replace('.md'))
+//                     .pipe(gulp.dest('lag'))
+//             });
+//
+//             file('teams.json', JSON.stringify(teams))
+//                 .pipe(convert({
+//                     from: 'json',
+//                     to: 'yml'
+//                 }))
+//                 // .pipe(console.log.bind(console))
+//                 .pipe(gulp.dest('_data'))
+//
+//         });
+//     })
+//     .catch(function(error) {
+//         console.log(new Error(error))
+//     })
+// })
+//
+//
+// gulp.task('clean:teams', function(cb) {
+//     del(['./lag/**', './_data/teams.yml'], cb);
+// });
+//
+// gulp.task('clean:contacts', function(cb) {
+//     del(['./_data/contacts.yml'], cb);
+// })
 
 gulp.task('default', ['browser-sync', 'watch']);
